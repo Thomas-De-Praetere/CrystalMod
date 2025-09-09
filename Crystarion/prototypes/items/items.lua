29fcf239fb0f6ad593a5696e2929269ef174ffe7
Thomas De Praetere <thomas.de.praetere@hotmail.com>
2025-09-09 10:08:19 +0200

    update icon locations

--- Crystarion/prototypes/items/items.lua
+++ Crystarion/prototypes/items/items.lua
@@ -1,19 +1,19 @@
 local crushed_crystal = table.deepcopy(data.raw["item"]["iron-plate"])
 
 crushed_crystal.name = "crystarion-crushed-crystal"
-crushed_crystal.icon = "__Crystarion__/graphics/items/crushed_crystal.png"
+crushed_crystal.icon = "__Crystarion__/graphics/items/crushed_crystal/crushed_crystal.png"
 crushed_crystal.icon_size = 64
 
 local crystal = table.deepcopy(data.raw["item"]["iron-plate"])
 
 crystal.name = "crystarion-crystal"
-crystal.icon = "__Crystarion__/graphics/items/crystal.png"
+crystal.icon = "__Crystarion__/graphics/items/crystal/crystal.png"
 crystal.icon_size = 64
 
 local crystal_ore = table.deepcopy(data.raw["item"]["iron-ore"])
 
 crystal_ore.name = "crystarion-crystal-ore"
-crystal_ore.icon = "__Crystarion__/graphics/items/crystal_ore.png"
+crystal_ore.icon = "__Crystarion__/graphics/items/crystal_ore/crystal_ore.png"
 crystal_ore.icon_size = 64
 
 local crystal_slurry = table.deepcopy(data.raw["fluid"]["water"])
