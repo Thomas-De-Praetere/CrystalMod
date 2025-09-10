2c50bf0a20121f669490b8b55af5633bdcd01d07
Thomas De Praetere <thomas.de.praetere@hotmail.com>
2025-09-10 11:35:19 +0200

    Added more graphics and the original files. Used multiple versions of things to add niceties.

--- Crystarion/prototypes/items/items.lua
+++ Crystarion/prototypes/items/items.lua
@@ -2,13 +2,24 @@ local crushed_crystal = table.deepcopy(data.raw["item"]["iron-plate"])
 
 crushed_crystal.name = "crystarion-crushed-crystal"
 crushed_crystal.icon = "__Crystarion__/graphics/items/crushed_crystal.png"
-crushed_crystal.icon_size = 64
+crushed_crystal.pictures =
+{
+    { size = 64, filename = "__Crystarion__/graphics/items/crushed_crystal.png",   scale = 0.5, mipmap_count = 4 },
+    { size = 64, filename = "__Crystarion__/graphics/items/crushed_crystal_1.png", scale = 0.5, mipmap_count = 4 },
+    { size = 64, filename = "__Crystarion__/graphics/items/crushed_crystal_2.png", scale = 0.5, mipmap_count = 4 },
+    { size = 64, filename = "__Crystarion__/graphics/items/crushed_crystal_3.png", scale = 0.5, mipmap_count = 4 }
+}
 
 local crystal = table.deepcopy(data.raw["item"]["iron-plate"])
 
 crystal.name = "crystarion-crystal"
 crystal.icon = "__Crystarion__/graphics/items/crystal.png"
-crystal.icon_size = 64
+crystal.pictures =
+{
+    { size = 64, filename = "__Crystarion__/graphics/items/crystal.png",   scale = 0.5, mipmap_count = 4 },
+    { size = 64, filename = "__Crystarion__/graphics/items/crystal_1.png", scale = 0.5, mipmap_count = 4 },
+    { size = 64, filename = "__Crystarion__/graphics/items/crystal_2.png", scale = 0.5, mipmap_count = 4 }
+}
 
 local crystal_ore = table.deepcopy(data.raw["item"]["iron-ore"])
 
@@ -17,16 +28,15 @@ crystal_ore.icon = "__Crystarion__/graphics/items/crystal_ore.png"
 crystal_ore.pictures =
 {
     { size = 64, filename = "__Crystarion__/graphics/items/crystal_ore.png",   scale = 0.5, mipmap_count = 4 },
-    { size = 64, filename = "__Crystarion__/graphics/items/crystal_ore-1.png", scale = 0.5, mipmap_count = 4 },
-    { size = 64, filename = "__Crystarion__/graphics/items/crystal_ore-2.png", scale = 0.5, mipmap_count = 4 },
-    { size = 64, filename = "__Crystarion__/graphics/items/crystal_ore-3.png", scale = 0.5, mipmap_count = 4 }
+    { size = 64, filename = "__Crystarion__/graphics/items/crystal_ore_1.png", scale = 0.5, mipmap_count = 4 },
+    { size = 64, filename = "__Crystarion__/graphics/items/crystal_ore_2.png", scale = 0.5, mipmap_count = 4 }
 }
 
 local crystal_slurry = table.deepcopy(data.raw["fluid"]["water"])
 
 crystal_slurry.name = "crystarion-crystal-slurry"
-crystal_slurry.icon = "__Crystarion__/grpahics/items/slurry.png"
-crystal_slurry.base_color = { 0.8, 0.5, 0.4 }
-crystal_slurry.flow_color = { 0.8, 0.6, 0.6 }
+crystal_slurry.icon = "__Crystarion__/graphics/items/slurry.png"
+crystal_slurry.base_color = { 0.5, 0.7, 0.8 }
+crystal_slurry.flow_color = { 0.7, 1.0, 1.0 }
 
 data:extend({ crushed_crystal, crystal, crystal_ore, crystal_slurry })
