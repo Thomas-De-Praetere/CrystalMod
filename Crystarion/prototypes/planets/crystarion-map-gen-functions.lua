679792cde3a2582e13edcdf9ad00a43d7bc7e5c5
Thomas De Praetere <thomas.de.praetere@hotmail.com>
2025-10-11 22:29:26 +0200

    Added more map gen functions

--- Crystarion/prototypes/planets/crystarion-map-gen-functions.lua
+++ Crystarion/prototypes/planets/crystarion-map-gen-functions.lua
@@ -2,96 +2,107 @@ data:extend({
     --Constants
     {
         type = "noise-expression",
-        name = "crystarion_sea_level",
-        expression = '20'
-    },
-    {
-        type = "noise-expression",
         name = "crystarion_snow",
-        expression = '50'
+        expression = '33'
     },
     {
         type = "noise-expression",
         name = "crystarion_rich_snow",
+        expression = '66'
+    },
+    {
+        type = "noise-expression",
+        name = "crystarion_volc_start",
+        expression = '20'
+    },
+    {
+        type = "noise-expression",
+        name = "crystarion_slope",
+        expression = '60'
+    },
+    {
+        type = "noise-expression",
+        name = "crystarion_ridge",
         expression = '80'
     },
     {
         type = "noise-expression",
-        name = "crystarion_dirty_snow",
+        name = "crystarion_crater",
         expression = '90'
     },
+    --Helper
     {
-        type = "noise-expression",
-        name = "crystarion_ashy_snow",
-        expression = '100'
+        --Scales a function from [-1;1] to [0,100]
+        type = "noise-function",
+        parameters = { "to_scale" },
+        name = "crystarion_scale",
+        expression = 'to_scale * 50 + 50'
     },
     {
-        type = "noise-expression",
-        name = "crystarion_volcanic_base",
-        expression = '110'
-    },
-    {
-        type = "noise-expression",
-        name = "crystarion_volcanic_wall",
-        expression = '130'
-    },
-    {
-        type = "noise-expression",
-        name = "crystarion_crater_ridge",
-        expression = '140'
-    },
-    {
-        type = "noise-expression",
-        name = "crystarion_crater",
-        expression = '150'
+        --Scales a function from [-1;1] to [0,100]
+        type = "noise-function",
+        parameters = { "to_scale", "max_vals" },
+        name = "crystarion_scale_max",
+        expression = 'to_scale * max_vals/2 + max_vals/2'
     },
     --Height
     {
         type = "noise-expression",
         name = "crystarion_height_wobble",
-        expression = 'multioctave_noise{x = x, y = y, persistence = 0.75, seed0 = map_seed, seed1 = 0, octaves = 3, input_scale = 1/5}*2'
+        expression = 'multioctave_noise{x = x, y = y, persistence = 0.75, seed0 = map_seed, seed1 = 0, octaves = 3, input_scale = 1/5} * 2'
     },
     {
         type = "noise-expression",
         name = "crystarion_height_wobble_large",
-        expression = 'multioctave_noise{x = x, y = y, persistence = 0.75, seed0 = map_seed, seed1 = 1, octaves = 8, input_scale = 1/20}*50+50'
+        expression = 'crystarion_scale(multioctave_noise{x = x, y = y, persistence = 0.75, seed0 = map_seed, seed1 = 1, octaves = 8, input_scale = 1/20})'
     },
     {
         type = "noise-expression",
         name = "crystarion_volcans",
-        expression = "spot_noise{x = x,\z
+        expression = "clamp(spot_noise{\z
+                             x = x,\z
                              y = y,\z
                              seed0 = map_seed,\z
                              seed1 = 2,\z
-                             candidate_spot_count = 10,\z
-                             suggested_minimum_candidate_point_spacing = 128,\z
+                             candidate_spot_count = 5,\z
+                             suggested_minimum_candidate_point_spacing = 512,\z
                              skip_span = 3,\z
                              skip_offset = 0,\z
-                             region_size = 512,\z
-                             density_expression = 10,\z
-                             spot_quantity_expression = 1600,\z
-                             spot_radius_expression = 10,\z
+                             region_size = 2048,\z
+                             density_expression = 128,\z
+                             spot_quantity_expression = 1715000,\z
+                             spot_radius_expression = 128,\z
                              hard_region_target_quantity = 1,\z
                              spot_favorability_expression = crystarion_height_wobble_large,\z
                              basement_value = -1,\z
-                             maximum_spot_basement_radius = 10 * 2}"
+                             maximum_spot_basement_radius = 128 * 2\z
+                             },0,100)"
+    },
+    {
+        type = "noise-expression",
+        name = "crystarion_volcans_height_wobble",
+        expression = 'multioctave_noise{x = x, y = y, persistence = 0.75, seed0 = map_seed, seed1 = 2, octaves = 6, input_scale = 1/10}'
+    },
+    {
+        type = "noise-expression",
+        name = "crystarion_volcans_height",
+        expression = "(crystarion_volcans/2.0 + (crystarion_scale_max(crystarion_volcans_height_wobble,75))*crystarion_volcans/100)"
     },
     {
         type = "noise-expression",
         name = "crystarion_height",
-        --expression = 'max(crystarion_height_wobble_large + crystarion_height_wobble,crystarion_volcans)'
-        expression = 'max(crystarion_height_wobble_large,crystarion_volcans)'
+        expression = 'clamp(crystarion_height_wobble_large + crystarion_height_wobble,0,100)'
     },
     --Moisture
     {
         type = "noise-expression",
         name = "crystarion_moist_wobble_large",
-        expression = 'multioctave_noise{x = x, y = y, persistence = 0.75, seed0 = map_seed, seed1 = 3, octaves = 4, input_scale = 1/25}'
+        expression = 'crystarion_scale(multioctave_noise{x = x, y = y, persistence = 0.75, seed0 = map_seed, seed1 = 3, octaves = 5, input_scale = 1/100})'
     },
     {
         type = "noise-expression",
         name = "crystarion_moist",
-        expression = 'crystarion_moist_wobble_large*0.1 + 1.0 - clamp(crystarion_height,0.0,1.0)'
+        expression = 'crystarion_moist_wobble_large'
     },
     --Temperature
     {
@@ -104,59 +115,76 @@ data:extend({
         name = "crystarion_temp",
         expression = 'crystarion_temp_wobble'
     },
+    --Indication Functions
+    {
+        type = "noise-expression",
+        name = "crystarion_is_water",
+        --expression = 'if(crystarion_height < crystarion_moist, 1, 0)'
+        expression = 'if(crystarion_moist>75, 1, 0)'
+    },
+    {
+        type = "noise-expression",
+        name = "crystarion_is_ground",
+        expression = '(1 - crystarion_is_water)*(1 - crystarion_is_volcan)'
+    },
+    {
+        type = "noise-expression",
+        name = "crystarion_is_volcan",
+        --expression = 'crystarion_volcans > crystarion_height'
+        expression = 'crystarion_volcans_height > crystarion_height'
+    },
+
     --Tile spawn
     {
         type = "noise-expression",
         name = "crystarion_ice_rough",
-        expression = 'range_select(max(crystarion_height,crystarion_sea_level-1),crystarion_sea_level-1,crystarion_sea_level,0.1,-1,1)',
+        expression = '0',
     },
     {
         type = "noise-expression",
         name = "crystarion_ice_smooth",
-        expression = 'range_select(max(crystarion_height,crystarion_sea_level-1),crystarion_sea_level-1,crystarion_sea_level,0.1,-1,1)',
+        expression = 'crystarion_is_water',
     },
     {
         type = "noise-expression",
         name = "crystarion_snow_flat",
-        expression = 'range_select(crystarion_height,crystarion_sea_level,crystarion_snow,0.1,-1,1)',
+        expression = 'crystarion_is_ground*if(crystarion_height<crystarion_snow,1,0)',
     },
     {
         type = "noise-expression",
         name = "crystarion_snow_crests",
-        expression = 'range_select(crystarion_height,crystarion_snow,crystarion_rich_snow,0.1,-1,1)',
+        expression = 'crystarion_is_ground*range_select(crystarion_height,crystarion_snow,crystarion_rich_snow,0.1,-1,1)',
     },
     {
         type = "noise-expression",
         name = "crystarion_snow_lumpy",
-        expression = 'range_select(crystarion_height,crystarion_rich_snow,crystarion_dirty_snow,0.1,-1,1)',
+        expression = 'crystarion_is_ground*if(crystarion_height>crystarion_rich_snow,1,0)',
     },
     {
         type = "noise-expression",
         name = "crystarion_snow_patchy",
-        expression = 'range_select(crystarion_height,crystarion_dirty_snow,crystarion_ashy_snow,0.1,-1,1)',
+        expression = '0',
     },
     {
         type = "noise-expression",
         name = "crystarion_volcanic_pumice_stones",
-        expression = 'range_select(crystarion_height,crystarion_ashy_snow,crystarion_volcanic_base,0.1,-1,1)',
+        expression = 'crystarion_is_volcan*range_select(crystarion_volcans_height,0,crystarion_slope,0.1,-1,1)',
     },
     {
         type = "noise-expression",
         name = "crystarion_volcanic_ash_cracks",
-        expression = 'range_select(crystarion_height,crystarion_volcanic_base,crystarion_volcanic_wall,0.1,-1,1)',
+        expression = 'crystarion_is_volcan*range_select(crystarion_volcans_height,crystarion_slope,crystarion_ridge,0.1,-1,1)',
     },
     {
         type = "noise-expression",
         name = "crystarion_volcanic_folds",
-        expression = 'range_select(crystarion_height,crystarion_volcanic_wall,crystarion_crater_ridge,0.1,-1,1)',
+        expression = 'crystarion_is_volcan*range_select(crystarion_volcans_height,crystarion_ridge,crystarion_crater,0.1,-1,1)',
     },
     {
         type = "noise-expression",
         name = "crystarion_volcanic_cracks_hot",
-        expression = 'range_select(min(crystarion_height,crystarion_crater),crystarion_crater_ridge,crystarion_crater,0.1,-1,1)',
+        expression = 'crystarion_is_volcan*if(crystarion_volcans_height>crystarion_crater,1,0)',
     },
-
-
     --Entity spawn
     {
         type = "noise-expression",
