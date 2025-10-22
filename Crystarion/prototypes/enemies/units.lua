1b213104629ef98bfe7b908bfaa13d632195819f
Thomas De Praetere <thomas.de.praetere@hotmail.com>
2025-10-22 14:10:39 +0200

    Updated minable value of crain

--- Crystarion/prototypes/enemies/units.lua
+++ Crystarion/prototypes/enemies/units.lua
@@ -1,3 +1,5 @@
+local helper = require("__Crystarion__.helper")
+
 local unit = table.deepcopy(data.raw["unit"]["small-biter"])
 unit.name = "crystarion-biter"
 unit.dying_trigger_effect = {
@@ -5,4 +7,9 @@ unit.dying_trigger_effect = {
     check_buildability = false,
     entity_name = "crystarion-resource",
 }
+unit.vision_distance = 10
+unit.absorptions_to_join_attack = { crystarion_resonance = 4 }
+unit.distraction_cooldown = 50
+unit.min_pursue_time = helper.seconds() * 10
+unit.max_pursue_distance = 20
 data:extend({ unit })
--- Crystarion/prototypes/entities/resource_collector.lua
+++ Crystarion/prototypes/entities/resource_collector.lua
@@ -7,6 +7,7 @@ entity.crane.speed.arm.extension_speed = 0.01
 entity.crane.speed.grappler.vertical_turn_rate = 0.006
 entity.crane.speed.grappler.horizontal_turn_rate = 0.02
 entity.crane.speed.grappler.extension_speed = 0.02
+entity.minable = { mining_time = 0.2, result = "crystarion-collector" }
 
 local item = table.deepcopy(data.raw["item"]["agricultural-tower"])
 item.name = "crystarion-collector"
