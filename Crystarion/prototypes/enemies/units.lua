247888cea7a1d9f72e186039e989d8d02b9ed795
Thomas De Praetere <thomas.de.praetere@hotmail.com>
2025-11-22 11:04:12 +0100

    Added units splitting

--- Crystarion/prototypes/enemies/units.lua
+++ Crystarion/prototypes/enemies/units.lua
@@ -1,13 +1,19 @@
 local helper = require("__Crystarion__.helper")
 
-local function create_enemy(size, absorption, type)
+local function create_enemy(size, absorption, type, multiple, to_spawn)
+    local spawn_array = { { 0, 0 } }
+    if multiple then
+        spawn_array = { { 0, 1 }, { 0, -1 } }
+    end
+
     local unit = table.deepcopy(data.raw["unit"][size .. "-" .. type])
     unit.name = "crystarion-" .. size .. "-" .. type
     unit.dying_trigger_effect = {
         type = "create-entity",
         check_buildability = true,
         find_non_colliding_position = true,
-        entity_name = "crystarion-resource-" .. size,
+        entity_name = to_spawn,
+        offsets = spawn_array,
     }
     unit.vision_distance = 10
     unit.absorptions_to_join_attack = { crystarion_resonance = absorption }
@@ -22,12 +28,12 @@ end
 
 
 data:extend({
-    create_enemy("small", 4, "biter"),
-    create_enemy("medium", 20, "biter"),
-    create_enemy("big", 80, "biter"),
-    create_enemy("behemoth", 400, "biter"),
-    create_enemy("small", 4, "spitter"),
-    create_enemy("medium", 20, "spitter"),
-    create_enemy("big", 80, "spitter"),
-    create_enemy("behemoth", 400, "spitter"),
+    create_enemy("small", 4, "biter", false, "crystarion-resource-small"),
+    create_enemy("medium", 20, "biter", true, "crystarion-biter-small"),
+    create_enemy("big", 80, "biter", true, "crystarion-biter-medium"),
+    create_enemy("behemoth", 400, "biter", true, "crystarion-biter-big"),
+    create_enemy("small", 4, "spitter", false, "crystarion-resource-small"),
+    create_enemy("medium", 20, "spitter", false, "crystarion-resource-medium"),
+    create_enemy("big", 80, "spitter", false, "crystarion-resource-big"),
+    create_enemy("behemoth", 400, "spitter", false, "crystarion-resource-behemoth"),
 })
