26bf2f9255a5c14e14da9fa7ca9d856f76dbd7fb
Thomas De Praetere <thomas.de.praetere@hotmail.com>
2026-07-17 23:13:30 +0200

    faster shooting speed.

--- Crystarion/prototypes/offence/projectile_turret.lua
+++ Crystarion/prototypes/offence/projectile_turret.lua
@@ -20,7 +20,7 @@ entity.minable = { mining_time = 0.2, result = "crystarion-projectile-turret" }
 entity.attack_parameters = {
     type = "projectile",
     ammo_category = "crystarion-cannon-shell",
-    cooldown = 120,
+    cooldown = 60,
     projectile_creation_distance = 1,
     projectile_center = { 0, 0 },
     min_range = 15,
