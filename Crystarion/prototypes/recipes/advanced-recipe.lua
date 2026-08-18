bc7856b39fc128cd0ad255b8f46d10fa0c973cc2
Thomas De Praetere <thomas.de.praetere@hotmail.com>
2026-08-18 21:53:03 +0200

    added condense wood recipe.

--- Crystarion/prototypes/recipes/advanced-recipe.lua
+++ Crystarion/prototypes/recipes/advanced-recipe.lua
@@ -1,4 +1,5 @@
 -- locale recipe crystarion-crush-crystal-ore-adv
+-- locale recipe crystarion-condense-wood
 
 local helper = require("__Crystarion__.helper")
 
@@ -18,7 +19,25 @@ local crush_crystal_ore = {
     enabled = false,
 }
 
+local condense_wood = {
+    type = "recipe",
+    name = "crystarion-condense-wood",
+    categories = { "crystarion-empowerer-category" },
+    main_product = "crystarion-crystal",
+    ingredients = {
+        { type = "item", name = "wood", amount = 8 },
+        { type = "item", name = "ice", amount = 10 }
+    },
+    results = {
+        { type = "item", name = "crystarion-crystal-ore", amount = 8 },
+        { type = "item", name = "crystarion-crushed-crystal", amount = 4 },
+        { type = "item", name = "crystarion-crystal", amount = 1 }
+    },
+    energy_required = 4,
+    enabled = false,
+}
+
 
 data:extend({
-    crush_crystal_ore,
+    crush_crystal_ore, condense_wood
 })
